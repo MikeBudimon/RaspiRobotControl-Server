d62f4eab2da49d1a418f93e25946dec210c5ae24
Michal Wagner <mikebudimon@gmail.com>
2015-12-22 21:52:55 +0100

    - Now uses ServoBlaster library for more effective Software PWMs. - Improved Multi Threading code. - Optimized and cleaned code up.

--- src/de/raspirobotcontrol/DistanceMonitor.java
+++ src/de/raspirobotcontrol/DistanceMonitor.java
@@ -1,70 +1,50 @@
 package de.raspirobotcontrol;
 
-import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.PinState;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Measuring the distance to an object with a ultrasonic sensor
  */
 public class DistanceMonitor implements Runnable {
 
-
-    private final GpioPinDigitalInput pinEcho;  // receives impulses
-    private final GpioPinDigitalOutput pinTrigger; // sends impulses
     private final GpioPinDigitalOutput pinLedWarning; // distance Led status(red if too near)
     private boolean flag = false;
 
 
     // Constructor
-    protected DistanceMonitor(GpioController gpio, Pin pinTrigger, Pin pinEcho, Pin pinLedWarning) {
-        this.pinEcho = gpio.provisionDigitalInputPin(pinEcho);
-        this.pinTrigger = gpio.provisionDigitalOutputPin(pinTrigger, PinState.LOW);
+    protected DistanceMonitor(GpioController gpio, Pin pinLedWarning) throws InterruptedException {
         this.pinLedWarning = gpio.provisionDigitalOutputPin(pinLedWarning, PinState.LOW);
-
-        this.pinTrigger.setShutdownOptions(true, PinState.LOW);
         this.pinLedWarning.setShutdownOptions(true, PinState.LOW);
     }
 
 
     /**
-     * Measures distance to an object
+     * Measures distance to an object from a python script(usonic.py)
      *
      * @return Distance to an object in cm
      * @throws InterruptedException
      */
-    public double measureDistance() throws InterruptedException {
+    public String measureDistance() throws InterruptedException {
+        String s = null;
 
-        this.triggerSensor();
-        long starttime = System.nanoTime();
-        long endtime = System.nanoTime();
+        try {
+            Process process = Runtime.getRuntime().exec("python usonic.py");
+            BufferedReader brI = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            s = brI.readLine();
 
-        while (pinEcho.isLow()) {
-            starttime = System.nanoTime();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        while (pinEcho.isHigh()) {
-            endtime = System.nanoTime();
-        }
-
-        // time-difference in seconds
-        double timeDifference = (endtime - starttime);
-
-        // time-difference multiply with sonic speed 34300 cm/s and divide with 2 because forward and back
-        return (timeDifference / 1000000000 * 34300) / 2;
-
+        return s;
     }
-
-    /**
-     * Sends one impulse for 10 microseconds
-     *
-     * @throws InterruptedException
-     */
-    private void triggerSensor() throws InterruptedException {
-
-        pinTrigger.high();
-        Thread.sleep(0, 10000); // trigger duration of 10 micro s
-        pinTrigger.low();
-    }
-
 
     @Override
     public void run() {
@@ -73,22 +53,20 @@ public class DistanceMonitor implements Runnable {
 
         while (flag) {
             try {
-                System.out.printf("Object is %.1f cm away.\n", measureDistance());
-
                 // Sends the measured distance to the client when successfully connected.
                 if (Server.started) {
-                    Main.queue.put(String.valueOf(measureDistance()));
+                    String distance = measureDistance();
+                    Main.queue.put(distance);
+                    //System.out.println("Object is " + measureDistance() + " cm away.");
+
+                    // blink red led when distance is smaller than 10 cm
+                    if (Double.valueOf(distance) < 10.0) {
+                        pinLedWarning.blink(100, 1000);
+                    } else {
+                        pinLedWarning.low();
+                    }
+                    Thread.sleep(700);
                 }
-
-                // blink red led when distance is smaller than 25 cm
-                if (measureDistance() < 25.0) {
-                    pinLedWarning.blink(200, 1000);
-                } else {
-                    pinLedWarning.low();
-                }
-
-                Thread.sleep(1100);
-
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- src/de/raspirobotcontrol/Server.java
+++ src/de/raspirobotcontrol/Server.java
@@ -1,11 +1,11 @@
 package de.raspirobotcontrol;
 
+import com.pi4j.component.servo.ServoDriver;
+import com.pi4j.component.servo.ServoProvider;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinState;
-import com.pi4j.wiringpi.Gpio;
-import com.pi4j.wiringpi.SoftPwm;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -21,23 +21,28 @@ public class Server implements Runnable {
 
     static protected boolean started = false;
     private final GpioPinDigitalOutput buzzer;
-    private final int pin1, pin2, pin3, pin4;
     protected Thread runningThread = null;
+    private ServoDriver motorL_forw, motorL_bckw, motorR_bckw, motorR_forw;
     private DataOutputStream out = null;
     private int serverPort = 8080;
     private ServerSocket serverSocket = null;
     private boolean isStopped = false;
     private DataInputStream in = null;
 
-
-    public Server(int port, GpioController gpio, Pin buzzer, int pin1, int pin2, int pin3, int pin4) {
+    /**
+     * Servo cycle time:                20000us
+     * Pulse increment step size:       10us
+     * Minimum width value:             0 (0us) 0%
+     * Maximum width value:             2000 (20000us) 100%
+     */
+    public Server(int port, GpioController gpio, Pin buzzer, int pin1, int pin2, int pin3, int pin4, ServoProvider servoProvider) throws IOException {
         this.serverPort = port;
-        this.pin1 = pin1;
-        this.pin2 = pin2;
-        this.pin3 = pin3;
-        this.pin4 = pin4;
         this.buzzer = gpio.provisionDigitalOutputPin(buzzer, PinState.LOW);
         this.buzzer.setShutdownOptions(true, PinState.LOW);
+        this.motorL_forw = servoProvider.getServoDriver(servoProvider.getDefinedServoPins().get(pin1)); // P1-11: left forward
+        this.motorL_bckw = servoProvider.getServoDriver(servoProvider.getDefinedServoPins().get(pin2)); // P1-12: left backward
+        this.motorR_bckw = servoProvider.getServoDriver(servoProvider.getDefinedServoPins().get(pin3)); // P1-15: right backward
+        this.motorR_forw = servoProvider.getServoDriver(servoProvider.getDefinedServoPins().get(pin4)); // P1-16: right forward
     }
 
 
@@ -49,7 +54,6 @@ public class Server implements Runnable {
         synchronized (this) {
             this.runningThread = Thread.currentThread();
         }
-
         openServerSocket();
 
         while (!isStopped()) {
@@ -70,103 +74,90 @@ public class Server implements Runnable {
                 processClientRequest(clientSocket);
             } catch (Exception e) {
                 e.printStackTrace();
-
+            } finally {
+                if (started) {
+                    started = false;
+                    try {
+                        this.out.close();
+                        this.in.close();
+                        Main.queue.clear();
+                        stopPwms();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-
         }
-
         System.out.println("Server stopped.");
-
     }
 
     private void processClientRequest(Socket clientSocket) throws IOException, InterruptedException {
 
         System.out.println("Just connected to " + clientSocket.getRemoteSocketAddress());
 
-
-        // initialize wiringPi library
-        Gpio.wiringPiSetup();
-
-        // create soft-pwm pins (min=0 ; max=100)
-        SoftPwm.softPwmCreate(pin1, 0, 100); // P1-11: motor-left forward
-        SoftPwm.softPwmCreate(pin2, 0, 100); // P1-12: motor-left backward
-        SoftPwm.softPwmCreate(pin3, 0, 100); // P1-15: motor-right backward
-        SoftPwm.softPwmCreate(pin4, 0, 100); // P1-16: motor-right forward
-
+        Integer speed = 2000;   // Set raw value for servo drivers: 0 - 2000
         in = new DataInputStream(clientSocket.getInputStream());
         out = new DataOutputStream(clientSocket.getOutputStream());
         started = true;
-
-        String command;
+        String command = "5";
 
         label:
         while (started) {
-
-            command = in.readUTF();
+            if (Main.queue.size() > 0) out.writeUTF(Main.queue.take());
+            if (in.available() > 0) command = in.readUTF();
 
             switch (command) {
                 case "8":  // forward
-                    SoftPwm.softPwmWrite(0, 100);
-                    SoftPwm.softPwmWrite(1, 0);
-                    SoftPwm.softPwmWrite(3, 0);
-                    SoftPwm.softPwmWrite(4, 100);
-                    System.out.println("Forward!");
+                    motorL_forw.setServoPulseWidth(speed);
+                    motorR_forw.setServoPulseWidth(speed);
+                    motorL_bckw.setServoPulseWidth(0);
+                    motorR_bckw.setServoPulseWidth(0);
+                    //System.out.println("Forward!");
                     break;
-
                 case "2":  // backward
-                    SoftPwm.softPwmWrite(0, 0);
-                    SoftPwm.softPwmWrite(1, 100);
-                    SoftPwm.softPwmWrite(3, 100);
-                    SoftPwm.softPwmWrite(4, 0);
-                    System.out.println("Backward!");
+                    motorL_forw.setServoPulseWidth(0);
+                    motorR_forw.setServoPulseWidth(0);
+                    motorL_bckw.setServoPulseWidth(speed);
+                    motorR_bckw.setServoPulseWidth(speed);
+                    //System.out.println("Backward!");
                     break;
-
                 case "5":  // stop
-                    SoftPwm.softPwmWrite(0, 0);
-                    SoftPwm.softPwmWrite(1, 0);
-                    SoftPwm.softPwmWrite(3, 0);
-                    SoftPwm.softPwmWrite(4, 0);
-                    System.out.println("Stop!");
+                    stopPwms();
+                    //System.out.println("Stop!");
                     break;
-
                 case "4":  // left
-                    SoftPwm.softPwmWrite(0, 0);
-                    SoftPwm.softPwmWrite(1, 0);
-                    SoftPwm.softPwmWrite(3, 0);
-                    SoftPwm.softPwmWrite(4, 100);
-                    System.out.println("Left!");
+                    motorL_forw.setServoPulseWidth(0);
+                    motorR_forw.setServoPulseWidth(speed);
+                    motorL_bckw.setServoPulseWidth(speed);
+                    motorR_bckw.setServoPulseWidth(0);
+                    //System.out.println("Left!");
                     break;
-
                 case "6":  // right
-                    SoftPwm.softPwmWrite(0, 100);
-                    SoftPwm.softPwmWrite(1, 0);
-                    SoftPwm.softPwmWrite(3, 0);
-                    SoftPwm.softPwmWrite(4, 0);
-                    System.out.println("Right!");
+                    motorL_forw.setServoPulseWidth(speed);
+                    motorR_forw.setServoPulseWidth(0);
+                    motorL_bckw.setServoPulseWidth(0);
+                    motorR_bckw.setServoPulseWidth(speed);
+                    //System.out.println("Right!");
                     break;
-
                 case "0":  // audio-buzzer ON
-                    buzzer.pulse(2000, PinState.HIGH);
-                    System.out.println("Buzzer!");
+                    buzzer.pulse(1000, PinState.HIGH);
+                    //System.out.println("Buzzer!");
                     break;
-
                 case "stop": // stop connection
                     started = false;
                     this.out.close();
                     this.in.close();
                     Main.queue.clear();
+                    stopPwms();
                     break label;
-
                 default:
-                    System.out.println("Invalid enter!");
+                    stopPwms();
+                    //System.out.println("Invalid enter!");
                     break;
             }
-
-            out.writeUTF(Main.queue.take());
             Thread.sleep(100);
         }
     }
-
 
     private boolean isStopped() {
         return this.isStopped;
@@ -179,24 +170,27 @@ public class Server implements Runnable {
             this.in.close();
             out.close();
             this.serverSocket.close();
-
-            // shutdown all software-PWMs
-            SoftPwm.softPwmWrite(0, 0);
-            SoftPwm.softPwmWrite(1, 0);
-            SoftPwm.softPwmWrite(3, 0);
-            SoftPwm.softPwmWrite(4, 0);
-
-
+            stopPwms();
         } catch (IOException e) {
             throw new RuntimeException("Error closing server", e);
         }
+    }
+
+    /**
+     * shutdown all software-PWMs
+     */
+    private void stopPwms() {
+        motorL_forw.setServoPulseWidth(0);
+        motorR_forw.setServoPulseWidth(0);
+        motorL_bckw.setServoPulseWidth(0);
+        motorR_bckw.setServoPulseWidth(0);
     }
 
     private void openServerSocket() {
         try {
             this.serverSocket = new ServerSocket(this.serverPort);
         } catch (IOException e) {
-            throw new RuntimeException("Cannot open port 8080", e);
+            throw new RuntimeException("Cannot open port 9000", e);
         }
     }
 }
